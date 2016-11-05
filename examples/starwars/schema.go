c9fe67d98bc095d5d258e9d447d3e74f449615ed
nicerobot <nicerobot@users.noreply.github.com>
2016-11-04 17:56:56 -0700

    /test-compilation errors.

--- examples/starwars/schema.go
+++ examples/starwars/schema.go
@@ -2,6 +2,7 @@ package starwars
 
 import (
 	"errors"
+
 	"github.com/Spatially/graphql"
 	"github.com/Spatially/relay"
 	"golang.org/x/net/context"
@@ -118,9 +119,9 @@ func init() {
 				return nil, errors.New("Unknown node type")
 			}
 		},
-		TypeResolve: func(value interface{}, info graphql.ResolveInfo) *graphql.Object {
+		TypeResolve: func(info graphql.ResolveTypeParams) *graphql.Object {
 			// based on the type of the value, return GraphQLObjectType
-			switch value.(type) {
+			switch info.Value.(type) {
 			case *Faction:
 				return factionType
 			default:
